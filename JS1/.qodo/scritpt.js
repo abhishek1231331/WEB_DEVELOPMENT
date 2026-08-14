6cfdc42ceae7527efeba4b775086c02392caf0b6
abhishek1231331 <abhishekumar032006@gmail.com>
2026-08-14 23:38:29 +0530

    JAVASCRIPT

---
+++ JS1/.qodo/scritpt.js
@@ -0,0 +1,15 @@
+alert("hellow word");
+
+console.log("code is running");
+
+var a=prompt("enter a number");
+var isTrue=confirm("are you abhishekk kumar");
+
+if(isTrue){
+    console.log("computer is blasting");
+}
+
+else{
+    console.log("computer is not blastign");
+}
+console.log("your number is plus a");
