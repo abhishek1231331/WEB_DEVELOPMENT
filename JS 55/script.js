cfd715ffb6a5228106ab9175297280b1ddd256f0
abhishek1231331 <abhishekumar032006@gmail.com>
2026-08-15 19:10:34 +0530

    JAVASCRIPT

---
+++ JS 55/script.js
@@ -0,0 +1,43 @@
+console.log("hello abhishek")
+
+let a=5;
+
+// thats why we use let beacuse we can have changes in it.
+a=a+1;
+let b=9;
+let c="harry";
+let _a="shubham";
+
+// console.log(a+b+9);
+// console.log(typeof a,typeof b) ;
+{
+    console.log(a);
+}
+const a1=6;
+//we can not  change  constant thats why const.
+
+//const changing is not allowed because a1 is const
+
+let x="abhishek bhai";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;  //type of null--object.
+
+console.log(x,y,z,p,q,r)
+console.log(x,y,z,p,q,r)
+
+//an object is key value pair in javascript
+
+let o={
+    name:"harry"
+   
+
+}
+
+console.log(o);
+o.salary="100 crores";
+console.log(o);
+o.salary="500 crores";
+console.log(o);
