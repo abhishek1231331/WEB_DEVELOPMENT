8a63a8427b400fcecbb25b719bb72a1a87e32d7c
abhishek1231331 <abhishekumar032006@gmail.com>
2026-08-18 18:50:57 +0530

    js

---
+++ JS58/index.js
@@ -0,0 +1,23 @@
+function nice(name) {
+    console.log("hey" + " " + name + "you are nice")
+    console.log("hey" + " "+ name + "you are good")
+    console.log("hey"+" " + name + "you are excellent")
+}
+function sum(a,b) {
+    console.log(a+b)
+}
+
+console.log("hey harry you are nice")
+console.log("hey harry you are good")
+console.log("hey harry you are excellent")
+console.log("hey harry you are nice") 
+
+nice("harry")
+sum(3,5)
+
+const func1=(x)=> {
+    console.log("i am an arrow funciton",x)
+}
+
+
+func1(34);
---
+++ JS59/INDEX.js
@@ -0,0 +1,9 @@
+// 
+
+// create a faulty calculator 
+// 1-takes two user inputs.
+// 2-it performs operations as follow
+// +---- -
+// * ---- +
+// - ----- /
+// / ----- **
