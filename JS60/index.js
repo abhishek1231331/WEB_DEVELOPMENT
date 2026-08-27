fd6b20a83143b2c0ff5ccca0afe9829a9d75c8ac
abhishek1231331 <abhishekumar032006@gmail.com>
2026-08-27 21:03:12 +0530

    js

---
+++ JS60/index.js
@@ -0,0 +1,22 @@
+console.log("this is strings tutorial")
+
+let a="harry";
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+
+console.log(a.length)
+let name2="harry"
+let friend="rohan"
+console.log("his name is" + name2 +"and his friend name is" + friend)
+ 
+ 
+let b="shivam"
+console.log(b.toUpperCase())
+// console.log(b.tolowercase())
+// console.log(b.toLowercase())
+console.log(b.slice(1,4))
+console.log(b.replace("hiv","77"))
+console.log(b.concat(a,"Aiswarya")) 
